1631cfea428e5b1f9d2a41b8d582fcdd4fb80220
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-02-05 23:08:59 -0800

    Namespace to me.arrdem

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-auto "0.1.3"
+(defproject me.arrdem/lein-auto "0.1.4"
   :description "Leiningen plugin that executes tasks when files are modified"
   :url "https://github.com/weavejester/lein-auto"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
